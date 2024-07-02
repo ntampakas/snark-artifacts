f53679ed9dfc7971e6c94240340f15dccf813342
Nick Tampakas <nick@gmx.co.uk>
2024-07-02 16:14:50 +0300

    Dev

--- .github/scripts/update-cloudfront.sh
+++ .github/scripts/update-cloudfront.sh
@@ -5,12 +5,10 @@ artifacts=$(ls packages/ | grep -v 'artifacts\|cli')
 
 cd packages
 
-#for artifact in $artifacts; do
-for artifact in "drill"; do
+for artifact in $artifacts; do
   aws cloudfront get-function --name $artifact --stage LIVE output >/dev/null 2>&1
 
-  #package_latest_version=$(jq -r '.version' "$artifact/package.json")
-  package_latest_version="1.0.1-beta"
+  package_latest_version=$(jq -r '.version' "$artifact/package.json")
   cloudfront_current_version=$(egrep 'request.uri =' output | awk -F"/" '{ print $6 }')
 
   if [ $package_latest_version != $cloudfront_current_version ]; then
@@ -19,9 +17,9 @@ for artifact in "drill"; do
     etag=$(aws cloudfront describe-function --name $artifact --query 'ETag' --output text)
     aws cloudfront update-function --name $artifact --if-match $etag --function-config '{"Comment": "Update version", "Runtime": "cloudfront-js-2.0"}' --function-code fileb://output >/dev/null 2>&1
     echo "Updating $artifact function"
-    etag=$(aws cloudfront describe-function --name $artifact --query 'ETag' --output text)
-    aws cloudfront publish-function --name $artifact --if-match $etag >/dev/null 2>&1
-    echo "Publishing $artifact"
+    #etag=$(aws cloudfront describe-function --name $artifact --query 'ETag' --output text)
+    #aws cloudfront publish-function --name $artifact --if-match $etag >/dev/null 2>&1
+    #echo "Publishing $artifact"
   else
     echo "No changes applied for $artifact"
   fi
